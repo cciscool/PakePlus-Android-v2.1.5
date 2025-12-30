c80ad78bda43608d409ea538e3bc3ef1a0a4e810
CC <57086346+cciscool@users.noreply.github.com>
2025-12-30 16:04:48 +0800

    update src file from pakeplus

---
+++ scripts/www/static/constants.js
@@ -0,0 +1,12 @@
+export const User = {
+	name: '陈晓娟',
+	imageUrl: '../../static/logo.png',
+	position: '展业区主任七级',
+	companyName: '中国平安保险公司北京分公司',
+	idNumber: '1004817',
+	phone: '13848289892'
+}
+// export const baseUrl = 'http://47.99.33.16:9580/client/'
+//export const baseUrl = 'http://192.168.83.103:9580/client/'
+// export const baseUrl = 'http://localhost:9580/client/'
+export const baseUrl = 'http://120.27.231.112:9580/client/'
